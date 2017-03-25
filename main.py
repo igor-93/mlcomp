25a77f6dd27278dd501d277e488a4bbd5065516c
FlyingGibbon <mgruener@student.ethz.ch>
2017-03-25 18:06:01 +0100

    asdasd

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from load_data import load
 from visualize import vis
 from preprocess2_data import preprocess
-from classify_data import learn
+from classify_data import *
 
 import matplotlib.pyplot as plt
 
 
 def main():
-	X, Y = load(3000)
-	for i in range(2,4):
-		X_processed = preprocess(X, i)
-		learn(X_processed,Y)
+	X, y = load(1000)
+
+	X = preprocess(..)
+	classify_data.train(X,y)
+	X_test = load_test()
+	Y_test = classify_data.predict(X_test)
+	write_solution.write_output_classification(Y_test)
+	
+	big_images= load_data2()
+	boxes = extract_boxes(big_images) #big images
+	write_solution.write_output_detection(big_images,boxes)
+	
 	
 
 main()
