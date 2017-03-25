676e0e77db9e0bba0f969fcd6b313ec1bb4b4fe1
FlyingGibbon <mgruener@student.ethz.ch>
2017-03-25 14:26:07 +0100

    asdasd

---
+++ visualize.py
@@ -0,0 +1,18 @@
+from sklearn.manifold import LocallyLinearEmbedding as LLE
+from sklearn.decomposition import PCA 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def vis(X,Y):
+	L = LLE()#PCA(n_components =  2)
+	#L_vis = PCA(n_components = 100).fit(X)
+	X = L.fit_transform(X)
+	#eig = L_vis.explained_variance_ratio_
+	#print(eig)
+	fig, (low_dim,eigenvalues) = plt.subplots(2,1)
+	low_dim.scatter(X[:,0],X[:,1],c = Y)
+	#eigenvalues.plot(range(len(eig)),eig)
+	plt.show()
+	
+
