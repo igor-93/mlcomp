247bd9d065e824647fe003fb99eba3bf20c2c507
Lavrenti-Laptop\Lavrenti <lavrenti_frobeen@hotmail.de>
2017-03-25 18:04:03 +0100

    Included preprocessing step in load function

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from load_data import load
 from visualize import vis
 from preprocess2_data import preprocess
+from preprocess2_data import preprocess_image
 from classify_data import learn
 
 import matplotlib.pyplot as plt
 
 
 def main():
-	X, Y = load(3000)
-	for i in range(2,4):
-		X_processed = preprocess(X, i)
-		learn(X_processed,Y)
+	levels = 3
+
+	X, Y = load(3000, lambda x: preprocess_image(x, 0, levels))
+
+	learn(X, Y)
+
+	# for i in range(2,4):
+	# 	X_processed = preprocess(X, i)
+	# 	learn(X_processed,Y)
 	
 
 main()
