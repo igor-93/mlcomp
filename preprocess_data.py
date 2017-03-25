95f758ad37067958de9c107a3bd02988c740f2f3
FlyingGibbon <mgruener@student.ethz.ch>
2017-03-25 13:12:04 +0100

    finish ft extract

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,16 +1,16 @@
 import numpy as np
 from skimage.transform import pyramid_gaussian
 
-def preprocess(X, levels  = 5):
+def preprocess(X, levels  = 3):
 	w = len(X)
 	h = levels * 5
 	ft_matrix = np.zeros((w,h))
 	for idx,x in enumerate(X):
 		ft = []
-		pyramid = tuple(pyramid_gaussian(x))
+		pyramid = tuple(pyramid_gaussian(x,max_layer = levels))
 		for idx_img,img in enumerate(pyramid):
 			ft.append(extract_features(img))
-		ft_matrix[idx] = np.array(ft).flatten
+		ft_matrix[idx] = np.array(ft).flatten()
 	return ft_matrix
 
 
@@ -18,8 +18,7 @@ def extract_features(img):
 	
 	
 	w,h = img.shape
-	
-	ft = np.sum(img[:w//2,:]) - np.sum(img[w//2:,:])
+	ft = [np.sum(img[:w//2,:]) - np.sum(img[w//2:,:])]
 	ft.append(np.sum(img[:,:h//2]) - np.sum(img[:,h//2:]))
 	
 	sl =  np.sum(img[:w//3,:]) - np.sum(img[w//3:w//3*2,:]) + np.sum(img[w//3*2:,:])
